abfb6aa5437a3bb83f272c2f6c18ed8609af0591
LAViswhat <lav.iswhat@gmail.com>
2025-03-28 12:42:55 +0200

    fix: enhance drawer visibility and transition effects

--- src/components/ui/UiDrawer.vue
+++ src/components/ui/UiDrawer.vue
@@ -17,16 +17,16 @@ watch(
   <Teleport to="body">
     <div
       class="fixed inset-0 bg-black/50 transition-opacity lg:hidden z-40"
-      v-if="menuOpen"
+      v-show="menuOpen"
       @click="emit('close')"
     ></div>
   </Teleport>
 
   <!-- Drawer -->
   <aside
-    class="fixed top-0 right-0 w-64 h-full bg-stone-200 shadow-lg duration-500 transition-transform transform lg:hidden z-50"
+    class="fixed top-0 right-0 w-64 h-full bg-stone-200 shadow-lg duration-500 transition-transform transform lg:hidden z-[100]"
     v-show="menuOpen"
-    :class="{ 'translate-x-0': menuOpen, 'translate-x-full hidden': !menuOpen }"
+    :class="{ 'translate-x-0': menuOpen, 'translate-x-full': !menuOpen }"
   >
     <button class="absolute top-4 right-4 text-black-softer text-2xl" @click="emit('close')">
       ✕
@@ -38,15 +38,21 @@ watch(
 <style scoped>
 aside {
   height: 100vh;
-  -webkit-transition: transform 500ms ease-in-out;
-  transition: transform 500ms ease-in-out;
+  -webkit-transition:
+    transform 500ms ease-in-out,
+    opacity 500ms ease-in-out;
+  transition:
+    transform 500ms ease-in-out,
+    opacity 500ms ease-in-out;
+  background-color: #e7e5e4; /* Эквивалент bg-stone-200 */
+  box-shadow:
+    0 10px 15px -3px rgba(0, 0, 0, 0.1),
+    0 4px 6px -2px rgba(0, 0, 0, 0.05);
 }
 aside.translate-x-full {
-  visibility: hidden;
   opacity: 0;
 }
 aside.translate-x-0 {
-  visibility: visible;
   opacity: 1;
 }
 </style>
