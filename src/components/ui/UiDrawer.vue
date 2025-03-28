8d0816d3b26f0aa0c2db86b35302a222d044f887
LAViswhat <lav.iswhat@gmail.com>
2025-03-28 12:28:08 +0200

    fix: improve drawer visibility and transition effects

--- src/components/ui/UiDrawer.vue
+++ src/components/ui/UiDrawer.vue
@@ -25,7 +25,8 @@ watch(
   <!-- Drawer -->
   <aside
     class="fixed top-0 right-0 w-64 h-full bg-stone-200 shadow-lg duration-500 transition-transform transform lg:hidden z-50"
-    :class="{ 'translate-x-0': menuOpen, 'translate-x-full': !menuOpen }"
+    v-show="menuOpen"
+    :class="{ 'translate-x-0': menuOpen, 'translate-x-full hidden': !menuOpen }"
   >
     <button class="absolute top-4 right-4 text-black-softer text-2xl" @click="emit('close')">
       ✕
@@ -37,5 +38,15 @@ watch(
 <style scoped>
 aside {
   height: 100vh;
+  -webkit-transition: transform 500ms ease-in-out;
+  transition: transform 500ms ease-in-out;
+}
+aside.translate-x-full {
+  visibility: hidden;
+  opacity: 0;
+}
+aside.translate-x-0 {
+  visibility: visible;
+  opacity: 1;
 }
 </style>
